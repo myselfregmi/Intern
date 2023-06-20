9c7e5855c219c736311a709c596e45e925465eef
Pushpa <pushparegmi0@gmail.com>
2023-06-20 07:36:11 +0545

    Pizzamenu layout

--- Day 13/Content - Copy/Containerbox/src/App.jsx
+++ Day 13/Content - Copy/Containerbox/src/App.jsx
@@ -10,38 +10,38 @@ const App = () => {
     <div className='Box'>
       <Container
         title='Italian pizza with tomatoes topping and leaves'
-        content=' '
+        
         imageSrc='https://media.istockphoto.com/id/1295773428/photo/pizza-with-cheese-isolated-on-white-background-clipping-path-full-depth-of-field.jpg?s=170667a&w=0&k=20&c=wuPTLqWXS1G3vvND_jW2brgdQy6TMd68rDsAzIGbM94='
         buttonText='BEST FOR ALL'
         symbol='+'
-        price='$25'
+        price='$25.14'
       />
 
       <Container
         title='Napiletana spicy pizza with mushroom and leaves'
-        content=' '
+        
         imageSrc='https://media.istockphoto.com/id/466149291/photo/italian-pizza-with-cherry-tomatoes.jpg?s=612x612&w=0&k=20&c=lVsLx2tntN48LmjLePZIZw1m68nD1bClP_0yi-ANUMQ='
         buttonText='TOP CHOICE'
         symbol='+'
-        price='$25'
+        price='$55.00'
       />
 
       <Container
         title='Calofonia pizza with leaves and olives'
-        content=''
+      
         imageSrc='https://media.istockphoto.com/id/1231133159/photo/delicious-hot-hawaiian-pizza-with-chicken-fillet-ham-pineapple-oregano-and-cheese-mozzarella.jpg?s=612x612&w=is&k=20&c=Z43L69loExhscYNX_niY795FEdGlvPTk-8PdQOxTzF4='
         buttonText='FOR KIDS'
         symbol='+'
-        price='$25'
+        price='$40.00'
       />
 
       <Container
         title='Italian pizza with tomato topping and leaves'
-        content=' '
+        
         imageSrc='https://media.istockphoto.com/id/975092926/photo/ham-and-sausage-pizza-on-white-background-copy-space-recipe-and-menu-top-view.jpg?s=612x612&w=is&k=20&c=TRlK0rJY6MY4vhtMSccXNPx5nE2Tna20T4Dq2zD8e0g='
         buttonText='BEST IN VEGAN'
         symbol='+'
-        price='$25'
+        price='$25.14'
       />
     </div>
   );
--- Day 13/Content - Copy/Containerbox/src/components/Container.jsx
+++ Day 13/Content - Copy/Containerbox/src/components/Container.jsx
@@ -6,14 +6,21 @@ import React from 'react';
 const Container = (props) => {
   return (
     <div className='container'>
-      <h2 className='title'>{props.title}</h2>
-      <p className='content'>{props.content}</p>
+    
+      <div className="imagesss">
+
       <img src={props.imageSrc} alt={props.title} className='image' />
-      <div className='price'>
-        <span className='symbol'>{props.symbol}</span>
-        <span className='amount'>{props.price}</span>
       </div>
+      <div className="text1">
+
+      <h2 className='title'>{props.title}</h2>
       <button className='button'>{props.buttonText}</button>
+      </div>
+      <div className='price'>
+       
+           <p>{props.symbol}</p>
+          <h9>{props.price}</h9> 
+      </div>
     </div>
   );
 };
